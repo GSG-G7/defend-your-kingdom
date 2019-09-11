3aa515d77503fc9a53acf465ac177ebd00ecda2b
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-11 15:21:27 +0300

    move invaders Relates #8

--- src/ClassComponents/Header/Header.js
+++ src/ClassComponents/Header/Header.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header__score">
-        <p className="score-num">Score: <span>0</span></p>
+        <p className="score-num">
+          Score: <span>0</span>
+        </p>
       </div>
       <div className="header__level">
         <h4 className="level-num">Level</h4>
         <p>1</p>
       </div>
       <div className="header__instructions">
-        <button className="instructions">Instructions</button>  
+        <button className="instructions">Instructions</button>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Header;
--- src/ClassComponents/Invaders/Invaders.js
+++ src/ClassComponents/Invaders/Invaders.js
@@ -1,11 +1,13 @@
 import React from "react";
+// import AnimateInvaders from "./AnimateInvaders";
 import "./Invaders.css";
 
-const Invaders = () => {
+const Invaders = ({ position }) => {
   const invaders = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
   return (
     <div className="invaders-box">
-      <div className="invaders">
+      <div className="invaders" style={{ top: position }}>
         {invaders.map(invader => (
           <div className="invader" key={invader}></div>
         ))}
